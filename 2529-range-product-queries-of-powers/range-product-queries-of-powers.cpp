da4787029cf024c38521f938a676dbeaccf2328f
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-11 12:20:05 +0530

    Time: 25 ms (50.53%) | Memory: 147.8 MB (45.21%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        int M = 1e9+7;
-        vector<int>power;
-        vector<int>result;
-        for(int i = 0; i < 32; i++){
-            if((n & (1 << i)) != 0){ // iska mtlb hai ki ye check krega position of
-                power.push_back(1 << i); // means 1 << i === pow(2, i);
-
+        int M = 1e9 + 7;
+        vector<int> power;
+        vector<int> result;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) { // iska mtlb hai ki ye check krega position of
+                power.push_back(pow(2, i)); // means 1 << i === pow(2, i);
             }
         }
-        for(auto &querie: queries){
+        for (auto& querie : queries) {
             int start = querie[0];
             int end = querie[1];
             long long prod = 1;
-            for(int i = start; i <= end; i++){
-                     prod = (prod * power[i]) % M;
+            for (int i = start; i <= end; i++) {
+                prod = (prod * power[i]) % M;
             }
             result.push_back(prod);
-
         }
         return result;
     }
