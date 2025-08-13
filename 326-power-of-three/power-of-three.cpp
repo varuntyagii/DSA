d6212e348a868265c0c621785c5a97222378043d
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-13 11:07:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (46.41%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool check(int n ){
-        if(n <= 0)return false;
         if( n == 1)return true;
-        if(n % 3 != 0)return false;
+        if(n <= 0 || n % 3 != 0)return false;
 
        return check(n/3);
     }
