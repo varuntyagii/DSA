94aaec2583ca29f43e9197187b90b0935a604bbe
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-16 20:02:45 +0530

    Time: 67 ms (18.60%) | Memory: 53.3 MB (15.16%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        
-        vector<int> t(n, false);
-        //t[i] = True means, you can reach index i
-        
-        t[0] = true; //Already at starting index
-        
-        for(int i = 1; i<n; i++) {
-            for(int j = i-1; j>=0; j--) {
-                if(j + nums[j] >= i && t[j]) { //here t[j] == true means you should be able to reach j also, 
-                                               //then only you can plan to jump(j+nums[i])  from this jth index
-                    t[i] = true;
+        vector<bool>dp(n, false);
+        dp[0] = true;
+        for(int i = 0; i < n; i++){
+            for(int j = i - 1; j >= 0; j--){
+                if(nums[j] + j >= i && dp[j] == true){
+                     dp[i] = true;
                     break;
                 }
+              
             }
         }
-        
-        
-        return t[n-1];
+        return dp[n - 1];
 
     }
 };
