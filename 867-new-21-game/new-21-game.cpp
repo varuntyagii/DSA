b87b5bcf098da5b131d791736d6b3ed84c87b661
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-17 23:42:52 +0530

    Time: 3 ms (61.18%) | Memory: 12.8 MB (33.18%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -5,13 +5,14 @@ public:
         p[0] = 1; //Alice starts with 0 points and draws numbers 
         double prob = (k == 0)? 0: 1;
         for(int i = 1; i <= n; i++){
-            p[i] = prob / maxPts;
+            p[i] = prob / maxPts; // secoond
             if(i < k){
                 prob += p[i];
             }
             if(i - maxPts >= 0 && i - maxPts < k){
-                prob -= p[i - maxPts];
+                prob -= p[i - maxPts]; // 21 - 10 = 11 first p(firs) * second
             }
+
         }
         double result = 0.0;
         for(int i = k; i <= n; i++){
@@ -20,3 +21,8 @@ public:
         return result;
     }
 };
+
+ // k = 17 n = 21 maxPts = 10;
+//p(21) = p(16 * 1 / MaxPts) + p(15 * 1 / maxPts) ... 
+//p(17) = p(16 * 1 / MaxPts) + p(15 * 1 / maxPts) ... p(7 * 1 / maxPts) 
+//p(16) = p(15 * 1 / MaxPts) + p(14 * 1 / maxPts) ... p(6 * 1 / maxPts) 
