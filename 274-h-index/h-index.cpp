f6b79606a0b1502c0adc539c788751e05eea4813
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-18 11:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (26.23%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(); 
+        vector<int>temp(n + 1, 0);
+        for(int c: citations){
+            if(c > n){
+                // 6 < 7 = temp[6] = 0 = 0++ = 1;
+                temp[n]++; //ex = n = 6 c = 7,8 vector mei uss jagah par tum uska count bdha do;
+            }
+            else{
+                //6 < 5 = 0, 0++ = 1 position at the index of 5 increased by 1;
+                temp[c]++;
+            }
+        }
+        int count = 0; 
+        for(int i = n; i >= 0; i--){
+                        count += temp[i];
+
+            if(count >= i){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+//Jitni baar alag-alag researchers tere paper ko reference denge, utni tere citations increase hote hain.
+// Aur ye hi H-index calculate karne me kaam aata hai.
