6179ffcc76b6a5ed9e78e88f335137b5bab0e74e
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-19 17:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 111.3 MB (74.50%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -4,17 +4,16 @@ public:
         int n = nums.size();
         int i = 0;
         long long result = 0;
-
-        while(i < n) {
-            if(nums[i] == 0) {
-                long long l = 0;
-                // count length of continuous zero block
-                while(i < n && nums[i] == 0) {
+        while (i < n) {
+            if (nums[i] == 0 && i < n) {
+                long long  l = 0;
+                while( i < n && nums[i] == 0) {
                     l++;
                     i++;
                 }
-                // add subarrays from this block
+
                 result += (l * (l + 1)) / 2;
+
             } else {
                 i++;
             }
