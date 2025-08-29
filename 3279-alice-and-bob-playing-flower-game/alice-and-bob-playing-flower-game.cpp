3ae9a85ce64727697ccb1876029358bb6a4d31a3
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-29 20:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    long long flowerGame(int n, int m) {
+    long long flowerGame(long long  n, long m) {
         // here u can obserb that when alice win that the sum of x + y = odd
         // means the value of x either odd or even ti give the odd sum value,
         // similarly for y;
-        return 1LL * ((n+1)/2) * (m/2) + 1LL * (n/2) * ((m+1)/2);  
-          }
+
+    return ((n + 1) / 2 * (m / 2) + (n / 2) * ((m + 1) / 2));
+    }
 };
