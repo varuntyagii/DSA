d6dbbf62c21625cad6afe7fa3aeb4b87045cd591
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-08-29 20:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -4,5 +4,6 @@ public:
         // here u can obserb that when alice win that the sum of x + y = odd
         // means the value of x either odd or even ti give the odd sum value,
         // similarly for y;
-            return 1LL * ((n+1)/2) * (m/2) + 1LL * (n/2) * ((m+1)/2);    }
+        return 1LL * ((n+1)/2) * (m/2) + 1LL * (n/2) * ((m+1)/2);  
+          }
 };
