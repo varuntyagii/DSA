c534266e53023fde4ec0bed00ddf357bae801f3b
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-09-28 17:35:50 +0530

    Time: 10 ms (30.97%) | Memory: 25.7 MB (14.34%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -5,7 +5,7 @@ public:
         int result = 0;
         if(n  < 3)return 0;
         sort(nums.begin(), nums.end());
-        for(int i = n - 1; i >= 2; i--){ // reverse krnege kyuki loop ke exit hone se phle tum iska lagrest element find krlo
+        for(int i = n-1 ; i >= 2; i--){ // reverse krnege kyuki loop ke exit hone se phle tum iska lagrest element find krlo
             int a = nums[i - 2];
             int b = nums[i - 1];
             int c = nums[i];
