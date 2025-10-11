4de0aedb11f15f8b9320b4563057b0ea06394b65
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-10-11 21:38:26 +0530

    Time: 358 ms (46.80%) | Memory: 229.7 MB (22.88%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     typedef long long Int;
     unordered_map<Int, Int> mp;
-    vector<Int> dp; 
+     vector<Int> dp; // dynamic DP array
     int n;
-
     int binarySearch(int i, vector<Int>& nums) {
-        int ans = n; // default if not found
+        int ans = nums.size(); // default if not found
         int low = i + 1;
-        int high = n - 1;
+        int high = nums.size() - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -21,27 +20,29 @@ public:
         }
         return ans;
     }
-
     Int solve(vector<Int>& nums, int i) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
+        if (i >= n)
+            return 0;
+        if(dp[i] != -1)return dp[i];
         Int skip = solve(nums, i + 1);
         int j = binarySearch(i, nums);
-        Int take = (Int)mp[nums[i]] * nums[i] + solve(nums, j);
+        Int take = (Int)mp[nums[i]] * nums[i] + solve(nums, j); //freq * nums[i]
         return dp[i] = max(take, skip);
     }
-
     long long maximumTotalDamage(vector<int>& power) {
-        mp.clear();
-        for (auto x : power) mp[x]++;
-
+        
+        for (auto x : power) {
+            mp[x]++;
+        }
+        // after that u can take the unique element then u can use the vector or
+        // set
         vector<Int> nums;
-        for (auto& it : mp) nums.push_back(it.first);
+        for (auto& it : mp) {
+            nums.push_back(it.first); // unique element
+        }
         sort(nums.begin(), nums.end());
         n = nums.size();
-
-        dp.assign(n, -1);
+         dp.assign(n, -1); // initialize DP with -1
         return solve(nums, 0);
     }
 };
