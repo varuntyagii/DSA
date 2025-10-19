5b612089a3fcda38c8c80dbb05bfc84e4734737c
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-10-19 22:01:45 +0530

    Time: 272 ms (31.86%) | Memory: 63.8 MB (76.55%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    unordered_set<string>visit;
+    string smallest = "";
+    void rotate(string &s, int b){
+        reverse(s.begin(), s.end());
+        reverse(s.begin(), s.begin() + b);
+        reverse(s.begin() + b, s.end());
+    }
+    void dfs(string& s, int a, int b){
+        if(visit.find(s) != visit.end()){
+            return;
+        }
+        visit.insert(s);
+        if(smallest > s){
+            smallest = s; // small value
+        }
+        string op1 = s;
+        for(int i = 1; i < op1.size(); i+=2){
+            op1[i] = ((op1[i] - '0' + a) % 10 ) + '0';
+        }
+        // pass the new string value which is add by a 
+        dfs(op1, a, b);
+        string op2 = s;
+        rotate(op2, b);
+        dfs(op2, a, b);
+
+    }
+    string findLexSmallestString(string s, int a, int b) {
+        smallest = s; // store the first value;
+        dfs(s, a, b);
+        return smallest;
+    }
+};
