0802c7795aa27a2e5352fd6cafaecf57445a50e8
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-11-21 11:08:18 +0530

    Time: 58 ms (5.08%) | Memory: 54.7 MB (10.19%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -1,10 +1,11 @@
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
-Array.prototype.last = function() {
-    if(this.length && this[this.length -1] == null)return null;
-    return  this[this.length -1] ?? -1;
+Array.prototype.last = function () {
+    if (this.length === 0) return -1;
+    return this[this.length - 1];
 };
+
 
 /**
  * const arr = [1, 2, 3];
