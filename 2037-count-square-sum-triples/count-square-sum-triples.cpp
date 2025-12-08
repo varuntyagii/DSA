36dbf75f6796ceaa2de6275ceba83ea0b3f10a8d
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2025-12-08 05:44:47 +0000

    Time: 10 ms (46.46%) | Memory: 7.8 MB (55.75%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int countTriples(int n) {
         int count = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-
-                for(int c = 1; c <= n; c++){
-                    if(i * i + j * j == c * c ){
-                        count++;
-                    }
-                }
+        for(int a = 1; a <= n; a++){
+            for(int b = 1; b <= n; b++){
+               int c = a * a + b * b;
+               int root = sqrt(c);
+               if(root <= n && root * root == c){
+                count++;
+               }
             }
         }
         return count;
