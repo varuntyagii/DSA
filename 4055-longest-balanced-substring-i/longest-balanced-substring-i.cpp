e4678e378708a6ac6cf63e668ea765681fe1977b
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-02-12 13:42:40 +0530

    Time: 200 ms (65.45%) | Memory: 25.4 MB (72.15%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -3,25 +3,31 @@ using namespace std;
 
 class Solution {
 public:
+    bool balance(vector<int>&freq){
+        int common = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] == 0)continue;
+            if  (common == 0){
+                common = freq[i];
+
+            }
+            else if(freq[i] != common){
+                return false;
+            }
+        }
+        return true;
+    }
     int longestBalanced(string s) {
         int n = s.size();
         int maxLen = 0;
 
         for(int i = 0; i < n; i++) {
-
-            unordered_map<char, int> mp;
-            int maxFreq = 0;
+            vector<int>freq(26, 0);
 
             for(int j = i; j < n; j++) {
-
-                mp[s[j]]++;
-                maxFreq = max(maxFreq, mp[s[j]]);
-
-                int distinct = mp.size();
-                int len = j - i + 1;
-
-                if(maxFreq * distinct == len) {
-                    maxLen = max(maxLen, len);
+                freq[s[j] - 'a']++;
+                if(balance(freq)){
+                    maxLen = max(maxLen, j - i + 1);
                 }
             }
         }
