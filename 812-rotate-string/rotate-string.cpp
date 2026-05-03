7e341fc5e4949240e1d9802292cb400081d97ef5
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-05-03 21:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (61.66%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,11 +3,11 @@ public:
     bool rotateString(string s, string goal) {
         if(s.length() != goal.length())return false;
          string c = s + s;
-       for(int i = 0; i < c.length(); i++){
+      
         if(c.find(goal) != -1){
             return true;
         }
-       }
+       
         return false;
     }
 };
