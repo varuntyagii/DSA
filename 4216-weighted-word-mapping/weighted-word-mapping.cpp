4d03037e0362ed71d6edca42d3d0229e5eb5a67c
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-06-13 13:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+         for(auto word: words){
+            int sum = 0;
+            for(auto ch: word){
+                sum += weights[ch - 'a']; // a - a + 1 = 1 
+            }
+                sum %= 26;
+            ans.push_back('z' - sum); 
+         }
+         return ans;
+    }
+};
