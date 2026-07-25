a137e2e242f57bea8a19daae501a5bde2694c2a3
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-07-25 23:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (89.66%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int prod = 1;
+        int val = 0;
+        int maxVal1 = -1;
+        int maxVal2 = -1;
+        int ans = 1;
+        while (n > 0) {
+            val = n % 10;
+           if(val >= maxVal1){
+            maxVal2 = maxVal1;
+            maxVal1 = val;
+           }
+           else if(val > maxVal2){
+            maxVal2 = val;
+           }
+            n /= 10;
+          
+        }
+        return maxVal1 * maxVal2;
+    }
+};
