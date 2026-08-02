6f478d65c6583d850c62a19d7d77fdde6bb0ec2d
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-02 19:40:00 +0530

    Time: 6 ms (35.88%) | Memory: 11.7 MB (34.97%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    int dp[501][501];
+    int solve(vector<int>& piles, int i, int j) {
+        if (i > j)
+            return 0;
+
+        if(dp[i][j] != -1)return dp[i][j];
+        int take = piles[i] - solve(piles, i + 1, j);
+        int skip = piles[j] - solve(piles, i, j - 1);
+
+        return dp[i][j] = max(take, skip);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(piles, 0, piles.size() - 1);
+        return ans > 0;
     }
 };
