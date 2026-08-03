3feaf0dd4722c4c6bcdf5f7559615fdccfbb7a34
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-03 20:11:02 +0530

    Time: 24 ms (67.80%) | Memory: 135.8 MB (89.99%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[50001];
+    int solve(vector<int>& stone, int i) {
+        int n = stone.size();
+        int res = INT_MIN;
+        if(dp[i] != -1)return dp[i];
+        if (i >= stone.size())
+            return 0;
+        res = max(res, stone[i] - solve(stone, i + 1));
+         if (i + 1 < n) res =
+            max(res, stone[i] + stone[i + 1] - solve(stone, i + 2));
+         if (i + 2 < n) res = max(
+            res, stone[i] + stone[i + 1] + stone[i + 2] - solve(stone, i + 3)); // Alice(i+1,i+2, i+3) - solve(bob) 
+        return dp[i] = res; 
+    }
+    string stoneGameIII(vector<int>& stone) {
+        memset(dp, -1, sizeof(dp));
+        int val = solve(stone, 0);
+        if (val > 0)
+            return "Alice";
+        else if (val < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+};
