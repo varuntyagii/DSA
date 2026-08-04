4bf5719c19ee49606003171f5cbc59728edbf4dd
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-04 10:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (8.08%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>list;
+       set<int>st(nums.begin(), nums.end());
+        int mx = INT_MIN;
+        int mn = INT_MAX;
+        for(int i = 0; i < n; i++){
+            mn = min(mn, nums[i]);
+            mx = max(mx, nums[i]);
+            // st.insert(nums[i]);
+        }
+        for(int i = mn; i <= mx; i++){
+            if(st.find(i) != st.end()){
+                continue;
+            }
+            list.push_back(i);
+        }
+        return list;
+    }
+};
