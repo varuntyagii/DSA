b0d48fade2307b20c78affa7389777294f38b768
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-10 18:25:21 +0530

    Time: 39 ms (67.82%) | Memory: 14 MB (56.63%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[100001];
+
+    bool solve(int n) {
+        if (n == 0)
+            return false;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        for (int i = 1; i * i <= n; i++) {
+
+            if (solve(n - i * i) == false) {
+                return dp[n] = true;
+            }
+        }
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+};
