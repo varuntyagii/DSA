fea2fe20c7ab4d5734ed582713ed3f607f8c68a0
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-10 19:02:54 +0530

    Time: 18 ms (57.68%) | Memory: 91.1 MB (64.60%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    int maximumBags(vector<int>& arr, vector<int>& rocks, int a) {
-        int m = arr.size();
-        int n = rocks.size();
-        // for(int i = 0; i < m; i++){
-        //     if(arr[i] > rocks[i]){
-               
-        //        if(a > 0){
-        //          rocks[i]++;
-        //        }
-        //         a--;
-        //     }
-        //     if(arr[i] == rocks[i]){
-        //         count++;
-        //     }
-
-        // }
-       vector<int>req(n);
-      for(int i = 0; i < m; i++){
-        req[i] = arr[i] - rocks[i];
-       }
-       sort(req.begin(), req.end());
-       int count = 0;
-       for(int i = 0; i < req.size(); i++){
-        if(req[i] == 0){
-            count++;
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        vector<int>res(n);
+        for(int i = 0; i < n; i++){
+            res[i] = capacity[i] - rocks[i];
         }
-        else{
-            if(a >= req[i]){
-                a = a - req[i]; 
+        sort(res.begin(), res.end());
+        int count = 0;
+        for(int i = 0; i < res.size(); i++){
+            if(res[i] == 0){
                 count++;
             }
             else{
-                break;
+                if(additionalRocks >= res[i]){
+                    additionalRocks -= res[i];
+                    count++;
+                }
+                else{
+                    break;
+                }
+                
             }
         }
-       
-       }
-       return count;
+        return count;
     }
 };
