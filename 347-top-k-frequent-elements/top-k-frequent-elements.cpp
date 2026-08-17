d06c72bbaa59fcddf25eefeb42a3cb61a3f43b0b
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-17 11:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (45.14%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,24 +1,28 @@
+//vo k element return kro joki sbse jada aa rhe hai mtlb frequency ke basis par element return kro 
 class Solution {
 public:
-    typedef pair<int, int>p;
+    typedef pair<int,int>data;
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int>mp;
-        priority_queue<p, vector<p>, greater<p>>minHeap; // take using pair cause we traverse using freq og the map
-        for(auto &num: nums){
-            mp[num]++;
-        }
-        for(auto it: mp){
-        int value = it.first;
-        int freq = it.second;
-        minHeap.push({freq, value});
-            if(minHeap.size() > k)minHeap.pop();
-        }
-        vector<int>result;
-        while(!minHeap.empty()){
-            result.push_back(minHeap.top().second); // value
-            minHeap.pop();
-        }
-        return result;
+        //minheap
+       unordered_map<int, int>mp;
+       for(auto i: nums){
+        mp[i]++;
+       }
+       priority_queue<data, vector<data>, greater<data>>pq;
+
+       for(auto p: mp){
+        int value = p.first; // [1,1,1,2,2] 
+        int freq = p.second; // {1->3, 2->2}
+        pq.push({freq, value});
+        //if()
+        if(pq.size() > k)pq.pop();
+       }
+       vector<int>res;
+       while(!pq.empty()){
+        int num = pq.top().second; // value
+        res.push_back(num);
+        pq.pop();
+       }
+       return res;
     }
 };
