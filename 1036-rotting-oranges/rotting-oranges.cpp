87f93706293c5ca8bd0cccc38d34859126bd7a58
Varun Tyagi <143821152+varuntyagii@users.noreply.github.com>
2026-08-25 22:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.44%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    typedef pair<int,int>p;
+    vector<vector<int>>dir{{-1, 0}, {1, 0}, {0, - 1}, {0, 1}};
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<p>q;
+        int fresh = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 2){
+                    q.push({i, j});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+
+        if(fresh == 0)return 0;
+        int min = 0;
+        while(!q.empty()){
+            int k = q.size();
+            while(k--){
+                p curr = q.front();
+                q.pop();
+                int i = curr.first;
+                int j = curr.second;
+                for(auto d: dir){
+                    int newi= i + d[0]; // dir[0] = -1 
+                    int newj= j + d[1]; // dir[1] = 1 hume top jana hai or abhi hum grid[1][1] hai toh top jayenge
+                    if(newi >= 0 && newi < n && newj >= 0 && newj < m && grid[newi][newj] == 1){
+                        grid[newi][newj] = 2;
+                        q.push({newi, newj});
+                        fresh--;
+                    }
+
+                }
+            }
+            min++;
+        }
+        return fresh == 0 ? min - 1: -1;
+        
+    }
+};
